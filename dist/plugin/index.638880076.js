cccb5393ccd140b7d8fedebf0b1c11eaabed6908
MadGoby <stas.forostyuk@gmail.com>
2021-10-14 15:43:06 +0300

    #4 "dist/" added in .gitignore

--- dist/plugin/index.638880076.js
+++
@@ -1,9 +0,0 @@
-(self["webpackChunktest"] = self["webpackChunktest"] || []).push([["index.638880076"],{},
-/******/ __webpack_require__ => { // webpackRuntimeModules
-/******/ "use strict";
-/******/ 
-/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ __webpack_require__.O(0, ["commons"], () => (__webpack_exec__("./test/index.ts")));
-/******/ var __webpack_exports__ = __webpack_require__.O();
-/******/ }
-]);
--- dist/plugin/sliderSpecs.1280688032.js
+++
@@ -1,2 +0,0 @@
-(self["webpackChunktest"] = self["webpackChunktest"] || []).push([["sliderSpecs.1280688032"],{},
-0,[["./test/sliderSpecs.ts","runtime","commons"]]]);
