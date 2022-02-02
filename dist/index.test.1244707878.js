8b35cc0ddb5c1e8fdab516a442b5dbf81ccbc98b
MadGoby <stas.forostyuk@gmail.com>
2022-02-02 16:45:13 +0300

    GH-pages update

---
+++ dist/index.test.1244707878.js
@@ -0,0 +1,8 @@
+"use strict";
+(self["webpackChunktest"] = self["webpackChunktest"] || []).push([["index.test.1244707878"],{},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["commons"], () => (__webpack_exec__("./src/tools/index.test.ts")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
--- gh-pages/plugin/jquery.ui.customSlider.3385724518.js
+++
@@ -1,2 +0,0 @@
-(self["webpackChunktest"] = self["webpackChunktest"] || []).push([["jquery.ui.customSlider.3385724518"],{},
-0,[["./src/jquery.ui.customSlider.ts","runtime","commons"]]]);
