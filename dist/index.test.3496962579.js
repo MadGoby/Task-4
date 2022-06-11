35fdd78ded30cb79a5e29aab27874a80e45abd49
MadGoby <stas.forostyuk@gmail.com>
2022-06-11 18:47:36 +0300

    GH-pages update

--- dist/index.test.3496962579.js
+++ dist/index.test.3496962579.js
@@ -1,8 +1,8 @@
 "use strict";
-(self["webpackChunktest"] = self["webpackChunktest"] || []).push([["index.test.612610611"],{},
+(self["webpackChunktest"] = self["webpackChunktest"] || []).push([["index.test.3496962579"],{},
 /******/ __webpack_require__ => { // webpackRuntimeModules
 /******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ __webpack_require__.O(0, ["commons"], () => (__webpack_exec__("./src/plugin/tools/index.test.ts")));
+/******/ __webpack_require__.O(0, ["commons"], () => (__webpack_exec__("./src/plugin/utils/index.test.ts")));
 /******/ var __webpack_exports__ = __webpack_require__.O();
 /******/ }
 ]);
